41c025f0e72d986d62085c3b972a5f4868767113
Wojciech Karas <wkaras89@gmail.com>
2018-01-18 08:48:10 +0100

    refactor actions module

--- src/actions.ts
+++ src/actions.ts
@@ -1,30 +1,49 @@
-import { AnyFormSchema } from "./form"
+import { AnyFormSchema, FormSchema } from "./form"
 import { actionCreatorFactory } from "typescript-fsa"
+import { some, map } from "ts-object"
 
 const actionCreator = actionCreatorFactory("FORM")
 
-export const initializeForm = actionCreator<{
-  formSchema: AnyFormSchema
-}>("INITIALIZE")
+export const actions = {
+  initializeForm: actionCreator<{
+    formSchema: AnyFormSchema
+  }>("INITIALIZE"),
 
-export const changeFormField = actionCreator<{
-  value: any
-  field: string
-  formSchema: AnyFormSchema
-}>("CHANGE_FIELD")
+  changeFormField: actionCreator<{
+    value: any
+    field: string
+    formSchema: AnyFormSchema
+  }>("CHANGE_FIELD"),
 
-export const focusField = actionCreator<{
-  field: string
-  formSchema: AnyFormSchema
-}>("FOCUS_FIELD")
+  focusField: actionCreator<{
+    field: string
+    formSchema: AnyFormSchema
+  }>("FOCUS_FIELD"),
 
-export const blurField = actionCreator<{
-  field: string
-  formSchema: AnyFormSchema
-}>("BLUR_FIELD")
+  blurField: actionCreator<{
+    field: string
+    formSchema: AnyFormSchema
+  }>("BLUR_FIELD"),
 
-export const setFormFieldAsyncValidity = actionCreator.async<
-  { field: string; formSchema: AnyFormSchema },
-  { result: { [rule: string]: boolean } },
-  any
->("SET_FIELD_ASYNC_VALIDITY")
+  setFormFieldAsyncValidity: actionCreator.async<
+    { field: string; formSchema: AnyFormSchema },
+    { result: { [rule: string]: boolean } },
+    any
+  >("SET_FIELD_ASYNC_VALIDITY"),
+}
+
+export const isFormAction = (action: { type: string }): boolean =>
+  some((formAction) => formAction.type === action.type, actions)
+
+export const createFormActions = <TModel>(formSchema: FormSchema<TModel>) => ({
+  initializeForm: () => actions.initializeForm({ formSchema }),
+  fields: map(
+    (field, fieldName) => ({
+      changeFormField: (value: typeof field.initialValue) =>
+        actions.changeFormField({ field: fieldName, value, formSchema }),
+      focusField: () => actions.focusField({ field: fieldName, formSchema }),
+      blurField: () => actions.blurField({ field: fieldName, formSchema }),
+    }),
+    formSchema.fields,
+  ),
+})
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,10 @@ import { pipe } from "ts-function"
 import * as Forms from "./forms"
 import * as Form from "./form"
 
-import * as rawFormActions from "./actions"
-import { map } from "ts-object";
-
 export { asyncValidationMiddleware } from "./async-validation-middleware"
-
-export { FormSchema, FormState, FormInfo } from "./form"
+export { actions, createFormActions, isFormAction } from "./actions"
+export { formsReducer } from "./forms"
+export { FormSchema, FormState, FormInfo, formReducer } from "./form"
 
 export const getFormState = <Model>(
   formName: string,
@@ -20,17 +18,3 @@ export const getFormInfo = <Model>(
   store: Forms.FormsState,
 ): FormInfo<Model> =>
   pipe(store, Forms.getForm<Model>(formName), Form.getFormInfo)
-
-export const formsReducer = Forms.formsReducer
-
-export const actions = rawFormActions
-
-export const createFormActions = <TModel>(formSchema: Form.FormSchema<TModel>) => ({
-  initializeForm: () => actions.initializeForm({ formSchema }),
-  fields: map((field, fieldName) => ({
-    changeFormField: (value: typeof field.initialValue) =>
-      actions.changeFormField({ field: fieldName, value, formSchema }),
-    focusField: () => actions.focusField({ field: fieldName, formSchema }),
-    blurField: () => actions.blurField({ field: fieldName, formSchema }),
-  }) , formSchema.fields)
-})
